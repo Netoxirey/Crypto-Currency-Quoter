e99b1c62cbca89dbb939df9a4054fa2445eb5074
Netoxirey <ernestoserna94@gmail.com>
2023-04-06 21:14:09 -0600

    second commit

--- src/Components/CryptoCard.jsx
+++ src/Components/CryptoCard.jsx
@@ -14,6 +14,10 @@ const Img= styled.img `
 width: 100%;
 object-fit: contain;
 `
+
+const Span = styled.span `
+font-weight: bold;
+`
 const CryptoCard = () => {
 
     const {displayData} = useContext(CryptoContext)
@@ -25,12 +29,12 @@ const CryptoCard = () => {
             <Img src={`https://www.cryptocompare.com/${displayData.image}`} alt="crypto logo" />
             </div>
             <div>
-            <p>Name: {displayData.name}</p>
-            <p>Price: {displayData.price}</p>
-            <p>LastUpdate: {displayData.lastUpdate}</p>
-            <p>Higher Price Today: {displayData.higherPrice}</p>
-            <p>Lower Price Today: {displayData.lowerPrice}</p>
-            <p>24-Hour Price Change: {displayData.changue}</p>
+            <p>Name: <Span>{displayData.name}</Span></p>
+            <p>Price: <Span>{displayData.price}</Span></p>
+            <p>LastUpdate: <Span>{displayData.lastUpdate}</Span></p>
+            <p>Higher Price Today: <Span>{displayData.higherPrice}</Span></p>
+            <p>Lower Price Today: <Span>{displayData.lowerPrice}</Span></p>
+            <p>24-Hour Price Change: <Span>{displayData.changue}</Span></p>
             </div>
          
         </Card>: 'cargando'}
